070de936b9f941122a1b7a0f5a958c960a286c75
Khoa Hoang <koadahoakodah@gmail.com>
2023-07-13 00:09:52 -0400

    add plugins for working with rust

--- mappings.lua
+++ mappings.lua
@@ -9,4 +9,32 @@ M.general = {
 
 -- more keybinds!
 
+M.dap = {
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Toggle breakpoint",
+    },
+    ["<leader>dus"] = {
+      function ()
+        local widgets = require("dap.ui.widgets");
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar",
+    }
+  }
+}
+
+M.crates = {
+  n = {
+    ["<leader>rcu"] = {
+      function ()
+        require("crates").upgrade_all_crates()
+      end,
+      "Update crates",
+    }
+  }
+}
+
 return M
--- plugins.lua
+++ plugins.lua
@@ -46,6 +46,44 @@ local plugins = {
       require("better_escape").setup()
     end,
   },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
+    opts = function ()
+      return require "custom.configs.rust-tools"
+    end,
+    config = function(_, opts)
+      require("rust-tools").setup(opts)
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  }
 
   -- To make a plugin not be loaded
   -- {
