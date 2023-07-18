e0855d984cfb00ea467bacab9056aa011a6d9942
Khoa Hoang <koadahoakodah@gmail.com>
2023-07-18 02:15:09 -0400

    add some mappings

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,22 @@ local M = {}
 M.general = {
   n = {
     --[";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["J"] = { "mzJ`z", "Append next line to current" },
+    ["<C-k>"] = { "<cmd>cnext<CR>zz", "Next quickfix" },
+    ["<C-j>"] = { "<cmd>cprev<CR>zz", "Prev quickfix" },
+    ["<leader>k"] = { "<cmd>lnext<CR>zz", "Next location" },
+    ["<leader>j"] = { "<cmd>lprev<CR>zz", "Prev location" },
+    ["<leader>s"] = {
+      [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+      "Replace occurrences of current word",
+    },
+  },
+  v = {
+    ["J"] = { ":m '>+1<CR>gv=gv", "Move lines down" },
+    ["K"] = { ":m '<-2<CR>gv=gv", "Move lines up" },
+  },
+  x = {
+    ["<leader>p"] = { '"_dP', "Paste and replace" },
   },
 }
 
